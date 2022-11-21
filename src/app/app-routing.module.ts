6ee133fb218d203b47180a3c0ac9b58357f1868e
Patryk <pat.sieradzki@gmail.com>
2022-11-22 00:22:15 +0100

    ADD CATEGORY EX 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductCategoriesComponent } from './components/product-categories/product-categories.component';
+import { ProductListComponentModule } from './components/product-list/product-list.component-module';
+import { ProductsServiceModule } from './services/products.service-module';
+import { ProductCategoriesComponentModule } from './components/product-categories/product-categories.component-module';
+import { CategoriesServiceModule } from './services/categories.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([])],
+  imports: [RouterModule.forRoot([{
+    path: 'products', component: ProductListComponent }, { path: 'categories', component: ProductCategoriesComponent }]), ProductListComponentModule, ProductsServiceModule, ProductCategoriesComponentModule, CategoriesServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
